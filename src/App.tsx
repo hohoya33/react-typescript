2020b1e6e0f8a312ad958c9ca3f11e96f6221503
ho <hohoya33@gmail.com>
2018-04-15 01:22:42 +0900

    React TypeScript Tutorial 시작하기

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        React TypeScript Tutorial
+      </div>
+    );
+  }
+}
+
+export default App;
